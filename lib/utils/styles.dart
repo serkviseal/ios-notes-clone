78c87709814cb01ac8e595cb1c8c4420c6e8cfb7
Boris Kayi <boriskayienzo@gmail.com>
2021-02-13 03:40:24 +0200

    refactor:code cleaning

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-class CustomColors {
-  static Color yellow = CupertinoColors.systemOrange;
-}
-
 class CustomIconButton extends StatelessWidget {
   CustomIconButton({@required this.icon, @required this.onPressed});
   final Icon icon;
